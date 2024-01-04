e15095f24c2e5c3544c3c752affd2f048b4e4573
Nived Chandran <nived-c@industryapps.net>
2024-01-04 12:45:37 +0530

    Implemented new feature: <feature-description>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './home'
+import About  from './about';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About></About>
     </div>
   );
 }
---
+++ src/about.js
@@ -0,0 +1,12 @@
+// src/Product.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+       <h3> this is about page we are trying merge two branch</h3>
+    </div>
+  );
+};
+
+export default About;
